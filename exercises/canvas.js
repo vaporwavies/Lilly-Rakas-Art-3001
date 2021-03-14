06fbcb793a1bfc9b0ee1fbcaf8d68c6bc067b294
vaporwavies <77689221+vaporwavies@users.noreply.github.com>
2021-03-14 13:41:10 -0400

    yet another blogaroo

---
+++ exercises/canvas.js
@@ -0,0 +1,14 @@
+var imgObj;
+    var animate;
+    function init()
+    {imgObj=document.getElementById('shard');
+    imgObj.style.position= 'relative';
+    imgObj.style.left= '10px';
+    imgObj=document.getElementById('puff');
+    imgObj.style.position= 'relative';
+    imgObj.style.right= '10px';
+    }
+    function mouseinteract()
+    {
+
+    }
