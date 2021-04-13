eee0493a8b388c3e411fc4671ceacaaef557b71e
vaporwavies <77689221+vaporwavies@users.noreply.github.com>
2021-04-13 09:40:39 -0400

    donut room

--- exercises/newthing.js
+++ exercises/newthing.js
@@ -1,5 +1,16 @@
 const objects = [];
 let eyeZ;
+let img;
+
+
+function preload(){
+  
+  img = loadImage('donut.png');
+  gif = loadImage('donut.gif');
+  dots = loadImage('dots.gif');
+  noStroke();
+
+}
 
 function setup() {
   createCanvas(1200, 700, WEBGL);
@@ -12,49 +23,62 @@ function setup() {
   objects.push(new IntersectPlane(0, 1, 0, 0, 100, 0)); // Top wall
   objects.push(new IntersectPlane(0, 0, 1, 0, 0, 0)); // Back wall
 
+  img = loadImage('donut.png');
+  gif = loadImage('donut.gif');
+  dots = loadImage('dots.gif');
   noStroke();
   ambientMaterial(250);
 }
 
 function draw() {
-  background(0);
+
+  background(255); 
 
   // Lights
   let locX = mouseX +50;
   let locY = mouseY +50;
 
-  ambientLight(50);
+
+  ambientLight(100);
   directionalLight(200, 225, 235, 0.25, 0.25, 0);
-  pointLight(122, 40, 122, locX, locY, 250);
+  directionalLight(255, 255, 255, 0.5, 0.25, 0);
+  directionalLight(255, 255, 255, -.5, -0.5, -0.25);
+  pointLight(200, 120, 200, locX, locY, 250);
+
 
   // Left wall
+  texture(dots);
+
   push();
-  translate(-150, 0, 200);
+  translate(-200, 0, 200);
   rotateY((90 * PI) / 180);
-  plane(900, 300);
+  plane(900, 500);
   pop();
 
   // Right wall
   push();
-  translate(150, 0, 200);
+  translate(200, 0, 200);
   rotateY((90 * PI) / 180);
-  plane(900, 300);
+  plane(900, 500);
   pop();
 
   // Bottom wall
   push();
-  translate(0, 150, 200);
+  translate(0, 200, 200);
   rotateX((90 * PI) / 180);
-  plane(300, 900);
+  plane(500, 900);
   pop();
 
   // Top wall
   push();
-  translate(0, -150, 200);
+  translate(0, -200, 200);
   rotateX((90 * PI) / 180);
-  plane(300, 900);
+  plane(500, 900);
   pop();
 
+  texture(gif);
+  translate(0,0,-200);
+  plane(400, 400); // Back wall
 
   const x = mouseX - width / 2;
   const y = mouseY - height / 2;
@@ -76,14 +100,18 @@ function draw() {
     }
   }
 
+
   // Cursor
   push();
+  translate(0,0,200);
   translate(intersect);
   fill(210, 180, 180);
   rotateY((600-mouseX)/200);
   rotateX((350+mouseY)/200);
+  texture(img);
   torus(20);
   pop();
+  
 }
 
 
